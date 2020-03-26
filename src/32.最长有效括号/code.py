316664abaed16ee695e0bc7a0f4df7956f446ff3
Yang <yang.shen@commscope.com>
2020-03-26 12:53:08 +0800

    compose a pytest.

--- src/32.最长有效括号/code.py
+++ src/32.最长有效括号/code.py
@@ -9,4 +9,7 @@ class Solution:
     def longestValidParentheses(self, s: str) -> int:
         return 0
 # @lc code=end
+def test_answer():
+    sln = Solution()
+    assert sln.longestValidParentheses('') == 0
 
