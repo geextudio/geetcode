ae8c4cbe304b50faa805eb716151c47d6258917e
Yang SHEN <bizbetter@outlook.com>
2020-04-24 21:46:43 +0800

    file encoding for mac/linux

--- setupenv.cn.sh
+++ setupenv.cn.sh
@@ -1,4 +1,3 @@
-# Setup env for coding on node/python in China
-clear
+# setup env in China
 python3 -m venv geetcode
 yarn && source geetcode/bin/activate && python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip && python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r ./requirements.txt
--- setupenv.sh
+++ setupenv.sh
@@ -1,4 +1,3 @@
-# Setup env for coding on node/python
-clear
+# setup env 
 python3 -m venv geetcode
 yarn && source geetcode/bin/activate && python3 -m pip install --upgrade pip && python3 -m pip install -r ./requirements.txt
