6284220ba066591f437038bcba5659ea656a1f1d
Yang SHEN <geextudio@outlook.com>
2020-06-30 00:50:47 +0800

    add link to code

--- src/leetcode/python/problem19/code.py
+++ src/leetcode/python/problem19/code.py
@@ -2,7 +2,7 @@
 # @lc app=leetcode.cn id=19 lang=python3
 #
 # [19] 删除链表的倒数第N个节点
-#
+# https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
 
 # @lc code=start
 # Definition for singly-linked list.
