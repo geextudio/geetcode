d65f4595b8628177e1a29fea3719c3022505318c
Yang SHEN <geextudio@outlook.com>
2021-08-16 17:13:44 +0800

    init GeexInApp Xamarin project

---
+++ src/playground/dotnet/xamarin/GeexInApp/GeexInApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using GeexInApp.Services;
+using GeexInApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GeexInApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/playground/dotnet/xamarin/GeexInApp/GeexInApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GeexInApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GeexInApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
