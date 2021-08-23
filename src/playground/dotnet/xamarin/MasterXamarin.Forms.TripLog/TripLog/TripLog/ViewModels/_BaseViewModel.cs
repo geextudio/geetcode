4cf4add3ca659d132a8103ea0074cee4e9133283
Yang SHEN <geextudio@outlook.com>
2021-08-24 07:26:07 +0800

    add base viewmodel

---
+++ src/playground/dotnet/xamarin/MasterXamarin.Forms.TripLog/TripLog/TripLog/ViewModels/_BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TripLog.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged 
+    {
+        // INotifyPropertyChanged is key to behavior & role of the ViewModel and data binding.
+        // It allows a page to be notified when the properties of its ViewModel have changed.
+
+        public BaseViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
